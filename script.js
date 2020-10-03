3912ae6649723ee3175f32bb0a928d7a0e7a01cb
Manthan-1 <shahmanthan1303@gmail.com>
2020-10-03 16:33:51 +0530

    yayy

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ for (var i = 0; i < removeCartItemButtons.length; i++){
     var button = removeCartItemButtons[i]
     button.addEventListener('click', function(event){
         var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
     })
+}
+
+fucntion updateCartTotal(){
+    
 }
